8ecda6314570cdbe07a51791b30227fb59f10cfe
Shandelle Lovato <ShandelleLovato@gmail.com>
2018-02-19 17:21:59 -0700

    adds in my first try, for reference

--- hole_4.rb
+++ hole_4.rb
@@ -1,4 +1,13 @@
 # The method takes in a number and a max value then finds all multiples of the number up to the max value.
+#First try:
+# 
+# a = gets.strip.to_i
+# until a >= 100
+#   puts a
+#   a += a*2
+# end
+
+
 print "Type a number to be the base: "
 a = gets.strip.to_i
 print "Type a max number: "
